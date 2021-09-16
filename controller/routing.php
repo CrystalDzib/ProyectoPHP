acdf91cf7ba9f3f8bb670870e2ecb38cab3a43ff
CrystalDzib <89056187+CrystalDzib@users.noreply.github.com>
2021-09-16 18:23:06 -0500

    Add files via upload

---
+++ controller/routing.php
@@ -0,0 +1,24 @@
+<?php
+
+$var_getMenu = isset($_GET['menu']) ? $_GET['menu'] : 'inicio';
+echo "<br>";
+switch ($var_getMenu) {
+    case "bienvenida":
+        require_once ('./views/bienvenida.php');
+        break;
+  case "acercademi":
+    require_once('./views/acercademi.php');
+    break;
+    case "escuela":
+     require_once('./views/escuela.php');
+       break;
+   case "viajes":
+       require_once('./views/viajes.php');
+        break;
+    case "login":
+       require_once('./views/login.php');
+        break;
+    default:
+       require_once('./views/bienvenida.php');
+}
+?>
